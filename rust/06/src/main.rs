69315626e920b4e0adddba661a505069cfe4148c
mike <mpreilly@gatech.edu>
2024-12-08 15:29:21 -0500

    day 6 part 2

--- rust/06/src/main.rs
+++ rust/06/src/main.rs
@@ -4,8 +4,8 @@ use std::{collections::HashSet, fs};
 
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 struct Pos {
-    x: usize, 
-    y: usize
+    x: usize,
+    y: usize,
 }
 
 impl Pos {
@@ -14,31 +14,31 @@ impl Pos {
         if (self.x == 0 && change_x == -1) || (self.y == 0 && change_y == -1) {
             return None;
         }
-        
+
         Some(Pos {
             x: ((self.x as i32) + change_x) as usize,
-            y: ((self.y as i32) + change_y) as usize
+            y: ((self.y as i32) + change_y) as usize,
         })
     }
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 struct Guard {
     pos: Pos,
-    direction: (i32, i32) // if heading up, it's going +0 x, +1 y, so (0, 1)
+    direction: (i32, i32), // if heading up, it's going +0 x, +1 y, so (0, 1)
 }
 
 impl Guard {
     fn from_char(c: char, pos: Pos) -> Guard {
         Guard {
-            pos, 
+            pos,
             direction: match c {
                 '^' => (0, 1),
                 '>' => (1, 0),
                 'v' => (0, -1),
                 '<' => (-1, 0),
-                _ => panic!("unknown symbol")
-            }
+                _ => panic!("unknown symbol"),
+            },
         }
     }
 
@@ -53,7 +53,7 @@ impl Guard {
             (1, 0) => (0, -1),
             (0, -1) => (-1, 0),
             (-1, 0) => (0, 1),
-            _ => panic!("unknown direction")
+            _ => panic!("unknown direction"),
         }
     }
 
@@ -67,7 +67,7 @@ struct Map {
     guard: Guard,
     obstacles: HashSet<Pos>,
     max_x: usize,
-    max_y: usize
+    max_y: usize,
 }
 
 impl Map {
@@ -94,11 +94,16 @@ impl Map {
             guard: guard.unwrap(),
             obstacles,
             max_x: s.lines().next().unwrap().len() - 1,
-            max_y: s.lines().count() - 1
+            max_y: s.lines().count() - 1,
         }
     }
 
-    fn move_guard(&mut self) -> bool {
+    fn move_guard(&mut self, maybe_extra_obstacle: Option<Pos>) -> bool {
+        // if we're not actually using extra_obstacle, put it off the map
+        let extra_obstacle = maybe_extra_obstacle.unwrap_or(Pos {
+            x: self.max_x + 10,
+            y: self.max_y + 10,
+        });
 
         // at most 4 possible directions. maybe the guard is trapped (??)
         for _ in 0..4 {
@@ -106,7 +111,7 @@ impl Map {
                 if new_pos.x > self.max_x || new_pos.y > self.max_y {
                     return false;
                 }
-                if self.obstacles.contains(&new_pos) {
+                if self.obstacles.contains(&new_pos) || new_pos == extra_obstacle {
                     self.guard.rotate();
                 } else {
                     self.guard.advance();
@@ -125,7 +130,10 @@ impl Map {
 fn main() {
     let input = get_input(false);
     let map = Map::from_string(&input);
-    println!("{}", part1(map))
+    println!("{}", part1(map));
+
+    let map2 = Map::from_string(&input);
+    println!("{}", part2(map2));
 }
 
 fn get_input(toy: bool) -> String {
@@ -138,11 +146,51 @@ fn part1(mut map: Map) -> usize {
     guard_positions.insert(map.guard.pos);
 
     // seems like cycles are not possible
-    while map.move_guard() {
+    while map.move_guard(None) {
         guard_positions.insert(map.guard.pos);
     }
 
     guard_positions.len()
+}
+
+// diff approach: the grid isn't that big. Check all the positions
+fn part2(mut map: Map) -> usize {
+    let mut new_obstacle_positions: HashSet<Pos> = HashSet::new();
+    let start_guard_pos = map.guard.pos;
+
+    for x in 0..=map.max_x {
+        for y in 0..=map.max_y {
+            let pos = Pos { x, y };
+            if pos == start_guard_pos || map.obstacles.contains(&pos) {
+                continue;
+            }
+            if check_cycle(&mut map, pos) {
+                new_obstacle_positions.insert(pos);
+            }
+        }
+    }
+
+    new_obstacle_positions.len()
+}
+
+fn check_cycle(map: &mut Map, extra_obstacle: Pos) -> bool {
+    // need to put this back where we found it
+    let start_guard = map.guard;
+
+    let mut past_guard_states: HashSet<Guard> = HashSet::new();
+    past_guard_states.insert(map.guard);
+    // seems like cycles are not possible
+    while map.move_guard(Some(extra_obstacle)) {
+        if past_guard_states.contains(&map.guard) {
+            map.guard = start_guard;
+            return true;
+        }
+        past_guard_states.insert(map.guard);
+    }
+
+    map.guard = start_guard;
+
+    false
 }
 
 #[cfg(test)]
@@ -155,5 +203,13 @@ mod tests {
         let map = Map::from_string(&input);
         let result = part1(map);
         assert_eq!(result, 5453);
+    }
+
+    #[test]
+    fn part2_answer() {
+        let input = get_input(false);
+        let map = Map::from_string(&input);
+        let result = part2(map);
+        assert_eq!(result, 2188);
     }
 }
