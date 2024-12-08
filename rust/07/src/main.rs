33e2ca81070547e6990309b30d3b04e10d45fbd0
mike <mpreilly@gatech.edu>
2024-12-08 18:14:37 -0500

    day 7 part 2

--- rust/07/src/main.rs
+++ rust/07/src/main.rs
@@ -19,7 +19,8 @@ fn main() {
     let input = get_input(false);
     let equations: Vec<Equation> = input.lines().map(Equation::from_string).collect();
 
-    println!("part 1: {}", part1(&equations))
+    println!("part 1: {}", solution(&equations, false));
+    println!("part 2: {}", solution(&equations, true));
 }
 
 fn get_input(toy: bool) -> String {
@@ -27,23 +28,24 @@ fn get_input(toy: bool) -> String {
     fs::read_to_string(path).unwrap()
 }
 
-fn part1(equations: &[Equation]) -> u64 {
+fn solution(equations: &[Equation], is_part_2: bool) -> u64 {
     equations
         .iter()
-        .filter(|e| is_valid(e))
+        .filter(|e| is_valid(e, is_part_2))
         .map(|e| e.result)
         .sum()
 }
 
-fn is_valid(equation: &Equation) -> bool {
+fn is_valid(equation: &Equation, is_part_2: bool) -> bool {
     can_hit_result(
         equation.result,
         equation.operands[0],
         &equation.operands[1..],
+        is_part_2,
     )
 }
 
-fn can_hit_result(desired_result: u64, operand1: u64, remaining: &[u64]) -> bool {
+fn can_hit_result(desired_result: u64, operand1: u64, remaining: &[u64], is_part_2: bool) -> bool {
     if operand1 > desired_result {
         return false;
     }
@@ -53,8 +55,23 @@ fn can_hit_result(desired_result: u64, operand1: u64, remaining: &[u64]) -> bool
     }
 
     let operand2 = remaining[0];
-    can_hit_result(desired_result, operand1 + operand2, &remaining[1..])
-        || can_hit_result(desired_result, operand1 * operand2, &remaining[1..])
+    can_hit_result(
+        desired_result,
+        operand1 + operand2,
+        &remaining[1..],
+        is_part_2,
+    ) || can_hit_result(
+        desired_result,
+        operand1 * operand2,
+        &remaining[1..],
+        is_part_2,
+    ) || (is_part_2
+        && can_hit_result(
+            desired_result,
+            format!("{}{}", operand1, operand2).parse().unwrap(),
+            &remaining[1..],
+            is_part_2,
+        ))
 }
 
 #[cfg(test)]
@@ -65,7 +82,15 @@ mod tests {
     fn part1_answer() {
         let input = get_input(false);
         let equations: Vec<Equation> = input.lines().map(Equation::from_string).collect();
-        let result = part1(&equations);
+        let result = solution(&equations, false);
         assert_eq!(result, 1545311493300);
+    }
+
+    #[test]
+    fn part2_answer() {
+        let input = get_input(false);
+        let equations: Vec<Equation> = input.lines().map(Equation::from_string).collect();
+        let result = solution(&equations, true);
+        assert_eq!(result, 169122112716571);
     }
 }
