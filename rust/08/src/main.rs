c524c73c2a525808b45ebffacc82ff577349f767
mike <mpreilly@gatech.edu>
2024-12-08 20:09:09 -0500

    day 8 part 2

--- rust/08/src/main.rs
+++ rust/08/src/main.rs
@@ -1,17 +1,4 @@
-// bucket the antennas into lists by frequency.
-// within each frequency, consider all possible pairs of antennas.
-// for each pair:
-//   calculate their x/y distance.
-//   find the points that are that distance in the opposite directions
-//     d = dist(a, b) is (b.x - a.x, b.y - a.y)
-//     so resonance points are at (b.x + d.x, b.y + d.y)
-//       and (a.x - d.x, a.y - d.y)
-//   if they're within the bounds of the map, add to set
-
-// get unique combinations only (order doesn't matter)
-// for i in 0..(len - 1)
-//   for j in (i+1)..len (also avoids pairs with self)
-
+use gcd::Gcd;
 use std::{
     collections::{HashMap, HashSet},
     fs,
@@ -33,7 +20,9 @@ impl Pos {
 
     fn add(&self, dx: i32, dy: i32) -> Option<Pos> {
         // None if the move takes us negative
-        if (self.x == 0 && dx == -1) || (self.y == 0 && dy == -1) {
+        if (dx < 0 && self.x < dx.unsigned_abs() as usize)
+            || (dy < 0 && self.y < dy.unsigned_abs() as usize)
+        {
             return None;
         }
 
@@ -83,7 +72,8 @@ fn main() {
     let input = get_input(false);
     let map = Map::from_string(&input);
 
-    println!("{}", part1(&map));
+    println!("part 1: {}", part1(&map));
+    println!("part 2: {}", part2(&map));
 }
 
 fn get_input(toy: bool) -> String {
@@ -115,6 +105,57 @@ fn part1(map: &Map) -> usize {
     antinode_locations.len()
 }
 
+fn part2(map: &Map) -> usize {
+    let mut antinode_locations: HashSet<Pos> = HashSet::new();
+
+    for antennas in map.antennas_by_frequency.values() {
+        for (i, a1) in antennas[..(antennas.len() - 1)].iter().enumerate() {
+            for a2 in antennas[(i + 1)..].iter() {
+                let (dx, dy) = get_smallest_step_in_line(a1, a2);
+                add_points_in_line(map, &mut antinode_locations, a1, dx, dy, true);
+                add_points_in_line(map, &mut antinode_locations, a1, dx, dy, false);
+            }
+        }
+    }
+
+    antinode_locations.len()
+}
+
+// to get all that are "exactly in line", we should get the distances down to their smallest change.
+// e.g. (+1, -2) is the smallest we can go, because we can't divide that 1.
+// divide both changes by the greatest common factor.
+fn get_smallest_step_in_line(a1: &Pos, a2: &Pos) -> (i32, i32) {
+    let (dx, dy) = a1.dist(a2);
+    let gcd = (dx.unsigned_abs()).gcd(dy.unsigned_abs());
+    (dx / (gcd as i32), dy / (gcd as i32))
+}
+
+fn add_points_in_line(
+    map: &Map,
+    set: &mut HashSet<Pos>,
+    a1: &Pos,
+    dx: i32,
+    dy: i32,
+    forward: bool,
+) {
+    let direction = if forward { 1 } else { -1 };
+    let mut jump_num = 0;
+    while {
+        if let Some(antinode) = a1.add(dx * jump_num * direction, dy * jump_num * direction) {
+            if map.contains(&antinode) {
+                set.insert(antinode);
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    } {
+        jump_num += 1;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -125,5 +166,13 @@ mod tests {
         let map = Map::from_string(&input);
         let result = part1(&map);
         assert_eq!(result, 400);
+    }
+
+    #[test]
+    fn part2_answer() {
+        let input = get_input(false);
+        let map = Map::from_string(&input);
+        let result = part2(&map);
+        assert_eq!(result, 1280);
     }
 }
